1bc3988c5296abb0ddd3baa4a46cd7dd2aa99d01
DuaMajeed <160130460+Duamajeed00@users.noreply.github.com>
2024-09-14 01:43:40 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var buttons = document.querySelectorAll('.toggle-btn');
-    buttons.forEach(function (button) {
-        button.addEventListener('click', function () {
-            var target = button.getAttribute('data-target');
-            if (target) {
-                var content = document.querySelector("#".concat(target, ".content"));
-                if (content) {
-                    content.classList.toggle('hidden');
-                }
-            }
-        });
+// Function to toggle visibility of sections
+function toggleSection(targetId) {
+    var sectionContent = document.querySelector("#".concat(targetId, " .content"));
+    if (sectionContent) {
+        sectionContent.classList.toggle("hidden");
+    }
+}
+// Adding event listeners to all buttons
+document.querySelectorAll('.toggle-btn').forEach(function (button) {
+    button.addEventListener('click', function (event) {
+        var target = event.currentTarget.getAttribute('data-target');
+        if (target) {
+            toggleSection(target);
+        }
     });
 });
